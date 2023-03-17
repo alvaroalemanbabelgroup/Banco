9aed9574b47cb77c0d2d1c1aa04876f29f5793fa
Tomas Prados <tomas.prados@babelgroup.com>
2023-03-17 13:34:06 +0100

    Clase Sucursal Controller v2

--- Banco/src/main/java/com/example/Banco/controllers/SucursalController.java
+++ Banco/src/main/java/com/example/Banco/controllers/SucursalController.java
@@ -1,6 +1,11 @@
 package com.example.Banco.controllers;
 
+import com.example.Banco.models.Sucursal;
 import com.example.Banco.service.SucursalesService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 public class SucursalController {
     private SucursalesService sucursalesService;
@@ -9,5 +14,18 @@ public class SucursalController {
         this.sucursalesService = sucursalesService;
     }
 
+    @PostMapping("/alta")
+    public void alta(Sucursal sucursal){
+        this.sucursalesService.alta(sucursal);
+    }
 
+    @PostMapping("/modificar")
+    public void modificar(Sucursal sucursalVieja, Sucursal sucursalNueva){
+        this.sucursalesService.modificar(sucursalVieja, sucursalNueva);
+    }
+
+    @GetMapping("/listar")
+    public List<Sucursal> listar(){
+        return this.sucursalesService.listar();
+    }
 }
