efbb7e754abe0bdb5ee0ec357179bb20fd4253be
enriquemunozbabel <125457875+enriquemunozbabel@users.noreply.github.com>
2023-03-17 13:16:18 +0100

    Sucursal model

--- Banco/src/main/java/com/example/Banco/models/Sucursal.java
+++ Banco/src/main/java/com/example/Banco/models/Sucursal.java
@@ -1,4 +1,47 @@
 package com.example.Banco.models;
 
 public class Sucursal {
+    private int id;
+    private String nombre;
+    private String director;
+    private String direccion;
+
+    public Sucursal(int id, String nombre, String director, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.director = director;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
