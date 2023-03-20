78d922e4e16ede2f2a06f19d0abc0a60c57a2a12
Alvaro Aleman Rando <alvaro.aleman@babelgroup.com>
2023-03-20 09:28:35 +0100

    Cliente mapper y controller modificado

--- Banco/src/main/java/com/example/Banco/controllers/ClienteController.java
+++ Banco/src/main/java/com/example/Banco/controllers/ClienteController.java
@@ -1,4 +1,21 @@
 package com.example.Banco.controllers;
 
+import com.example.Banco.mappers.ClienteMapper;
+import com.example.Banco.models.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClienteController {
+    @Autowired
+    ClienteMapper clienteMapper;
+    @GetMapping("/cliente")
+
+    public ResponseEntity<Cliente> getCliente(){
+
+        Cliente cliente = this.clienteMapper.getCliente(3);
+        return ResponseEntity.ok(cliente);
+    }
 }
--- Banco/src/main/java/com/example/Banco/mappers/ClienteMapper.java
+++ Banco/src/main/java/com/example/Banco/mappers/ClienteMapper.java
@@ -1,9 +1,29 @@
 package com.example.Banco.mappers;
 
+import com.example.Banco.models.Cliente;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ClienteMapper {
-    
+    @Select("Select POLIZA_ID, NUM, NIF_TOMADOR from POLIZAMANAGER.POLIZAS where POLIZA_ID = #{idPoliza}")
+    @Results(value = {
+            @Result(property = "id", column = "POLIZA_ID"),
+            @Result(property = "numero", column = "NUM"),
+            @Result(property = "nifTomador", column = "NIF_TOMADOR")
+    })
+    Cliente getCliente(int idCliente);
+
+    @Select("Select id, email, dni from POLIZAMANAGER.POLIZAS")
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "numero", column = "dni"),
+            @Result(property = "email", column = "email")
+    })
+    List<Cliente> getPolizas();
 
 }
