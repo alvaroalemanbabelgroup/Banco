4357f5849e9ec61c6a2ad20519c30d7f778e43fa
Javier Roldan <javier.roldan@babelgroup.com>
2023-03-24 13:24:47 +0100

    clase sucursal completa

--- Banco/src/main/java/com/example/Banco/models/Sucursal.java
+++ Banco/src/main/java/com/example/Banco/models/Sucursal.java
@@ -3,20 +3,30 @@ package com.example.Banco.models;
 public class Sucursal {
     private int id;
     private String nombre;
-    private String director;
     private String direccion;
+    private String num;
+    private String piso;
+    private String puerta;
+    private String cod_postal;
+    private String municipio;
+    private String director;
 
-    public Sucursal(int id, String nombre, String director, String direccion) {
+
+    public Sucursal(int id, String nombre, String direccion, String num, String piso, String puerta, String cod_postal, String municipio, String director) {
         this.id = id;
         this.nombre = nombre;
-        this.director = director;
         this.direccion = direccion;
+        this.num = num;
+        this.piso = piso;
+        this.puerta = puerta;
+        this.cod_postal = cod_postal;
+        this.municipio = municipio;
+        this.director = director;
     }
 
     public int getId() {
         return id;
     }
-    
 
     public void setId(int id) {
         this.id = id;
@@ -30,19 +40,59 @@ public class Sucursal {
         this.nombre = nombre;
     }
 
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
     public String getDireccion() {
         return direccion;
     }
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public String getPuerta() {
+        return puerta;
+    }
+
+    public void setPuerta(String puerta) {
+        this.puerta = puerta;
+    }
+
+    public String getCod_postal() {
+        return cod_postal;
+    }
+
+    public void setCod_postal(String cod_postal) {
+        this.cod_postal = cod_postal;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
