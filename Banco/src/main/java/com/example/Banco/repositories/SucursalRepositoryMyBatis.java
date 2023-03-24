be270e8b3a032cb94e265a8bf2cc996a7426e4ba
Tomas Prados <tomas.prados@babelgroup.com>
2023-03-24 13:31:46 +0100

    @Options Repository

--- Banco/src/main/java/com/example/Banco/repositories/SucursalRepositoryMyBatis.java
+++ Banco/src/main/java/com/example/Banco/repositories/SucursalRepositoryMyBatis.java
@@ -11,19 +11,20 @@ public interface SucursalRepositoryMyBatis {
     //TODO: Implementar sentencia SQL con los nombres cuando los tengamos
     @Insert("INSERT INTO SUCURSAL(ID, NOMBRE, DIRECCION, NUM, PISO, PUERTA, COD_POSTAL, MUNICIPIO, DIRECTOR) " +
             "VALUES(#{sucursal.id}, #{sucursal.nombre}, #{sucursal.direccion}, #{sucursal.num}, #{sucursal.piso}," +
-            " #{sucursal.puerta}, #{sucursal.cod_postal}, #{sucursal.municipio}, #{sucursal.director},)")
+            " #{sucursal.puerta}, #{sucursal.cod_postal}, #{sucursal.municipio}, #{sucursal.director})")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "ID")
     public void alta(Sucursal sucursal);
 
 
     //TODO: Implementar/corregir sentencia SQL con los nombres cuando los tengamos
-    @Select("SELECT ID,NOMBRE,DIRECCION, NUM, PISO, PUERTA, COD_POSTAL, MUNICIPIO, DIRECTOR FROM SUCURSAL")
+    @Select("SELECT ID, NOMBRE, DIRECCION, NUM, PISO, PUERTA, COD_POSTAL, MUNICIPIO, DIRECTOR FROM SUCURSAL")
     public List<Sucursal> listar();
 
-    @Delete("DELETE FROM sucursal WHERE id=#{sucursal.id}")
+    @Delete("DELETE FROM SUCURSAL WHERE id=#{sucursal.id}")
     public void borrar(Sucursal sucursal);
 
 
-    @Update("Update SUCURSAL SET #{sucursalvieja.id}= #{sucursalnueva.id}, " +
+    @Update("UPDATE SUCURSAL SET #{sucursalvieja.id}= #{sucursalnueva.id}, " +
             "#{sucursalvieja.nombre}= #{sucursalnueva.nombre}," +
             "#{sucursalvieja.direccion}= #{sucursalnueva.direccion}," +
             "#{sucursalvieja.num}= #{sucursalnueva.num}," +
@@ -32,6 +33,6 @@ public interface SucursalRepositoryMyBatis {
             "#{sucursalvieja.cod_postal}= #{sucursalnueva.cod_postal} " +
             "#{sucursalvieja.municipio}= #{sucursalnueva.municipio} " +
             "#{sucursalvieja.director}= #{sucursalnueva.director} " +
-            "from SUCURSAL where id = #{sucursalvieja}")
+            "FROM SUCURSAL WHERE id = #{sucursalvieja}")
     public void modificar(Sucursal sucursalvieja, Sucursal sucursalNueva);
 }
