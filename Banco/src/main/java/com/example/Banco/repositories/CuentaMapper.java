6734d4178a7bc21d04c4d931bdcdc72c38affdbd
RafaVera <rafael.vera@babelgroup.com>
2023-03-27 09:24:54 +0200

    cambiado ID por ID_CUENTA

--- Banco/src/main/java/com/example/Banco/repositories/CuentaMapper.java
+++ Banco/src/main/java/com/example/Banco/repositories/CuentaMapper.java
@@ -12,31 +12,31 @@ import java.util.Optional;
 @Mapper
 public interface CuentaMapper {
 
-    @Select("Select ID, NUMERO_CUENTA, ID_CLIENTE, BALANCE from BANCO.CUENTA")
+    @Select("Select ID_CUENTA, NUMERO_CUENTA, ID_CLIENTE, BALANCE from BANCO.CUENTA")
     @Results(value = {
-            @Result(property = "idCuenta", column = "ID"),
+            @Result(property = "idCuenta", column = "ID_CUENTA"),
             @Result(property = "numeroCuenta", column = "NUMERO_CUENTA"),
             @Result(property = "idCliente", column = "ID_CLIENTE"),
             @Result(property = "balance", column = "BALANCE")})
     List<Cuenta> findAll();
 
-    @Select("Select ID, NUMERO_CUENTA, ID_CLIENTE, BALANCE from BANCO.CUENTA WHERE ID = #{id}")
+    @Select("Select ID_CUENTA, NUMERO_CUENTA, ID_CLIENTE, BALANCE from BANCO.CUENTA WHERE ID = #{id}")
     @Results(value = {
-            @Result(property = "idCuenta", column = "ID"),
+            @Result(property = "idCuenta", column = "ID_CUENTA"),
             @Result(property = "numeroCuenta", column = "NUMERO_CUENTA"),
             @Result(property = "idCliente", column = "ID_CLIENTE"),
             @Result(property = "balance", column = "BALANCE")})
     Optional<Cuenta> findById(int id);
 
-    @Select("Select ID, NUMERO_CUENTA, sucursal, ID_CLIENTE, BALANCE from BANCO.CUENTA WHERE ID = #{numero_cuenta}")
+    @Select("Select ID_CUENTA, NUMERO_CUENTA, sucursal, ID_CLIENTE, BALANCE from BANCO.CUENTA WHERE ID_CUENTA = #{numero_cuenta}")
     @Results(value = {
-            @Result(property = "numeroCuenta", column = "ID"),
+            @Result(property = "numeroCuenta", column = "ID_CUENTA"),
             @Result(property = "nombreSucursal", column = "sucursal"),
             @Result(property = "idUsuario", column = "ID_CLIENTE"),
             @Result(property = "balance", column = "BALANCE")})
     Optional<Cuenta> findByCuenta(int id);
 
     @Insert("INSERT INTO CUENTA (NUMERO_CUENTA, ID_CLIENTE, BALANCE) VALUES (#{numeroCuenta, jdbcType = VARCHAR}, #{idCliente}, #{balance})")
-    @Options(useGeneratedKeys = true, keyProperty = "idCuenta", keyColumn="ID")
+    @Options(useGeneratedKeys = true, keyProperty = "idCuenta", keyColumn="ID_CUENTA")
     Cuenta addCuenta(Cuenta cuenta);
 }
